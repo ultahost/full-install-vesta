b27c586831403eaaf3bade05aa05808740688f4a
UltaHost <52377533+ultahost@users.noreply.github.com>
2020-08-22 13:29:53 +0300

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-rm -Rfv /etc/yum.repos.d/CentOS-Vault.repo &> /dev/null
+PASSWD=$(openssl rand -base64 12)
+DOMAIN=$1
+IP=$(curl ifconfig.me)
+DIG_IP=$(getent ahostsv4 $DOMAIN | sed -n 's/ *STREAM.*//p')
 
-curl -O http://vestacp.com/pub/vst-install.sh && bash vst-install.sh --nginx yes --apache yes --phpfpm no --named yes --remi yes --vsftpd yes --proftpd no --iptables yes --fail2ban yes --quota no --exim yes --dovecot yes --spamassassin no --clamav no --softaculous no --mysql yes --postgresql no
+rm -Rfv /etc/yum.repos.d/CentOS-Vault.repo &> /dev/null
+echo $DOMAIN
+echo $PASSWDD
+hostnamectl set-hostname $DOMAIN
+curl -O http://vestacp.com/pub/vst-install.sh && bash vst-install.sh --nginx yes --apache yes --phpfpm no --named yes --remi yes --vsftpd yes --proftpd no --iptables yes --fail2ban yes --quota no --exim yes --dovecot yes --spamassassin no --clamav no --softaculous no --mysql yes --postgresql no --hostname $DOMAIN --email admin@$DOMAIN --password $PASSWD
 
 echo Installation will take about 5 minutes ...
 
-rpm --import http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro &> /dev/null
-rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm &> /dev/null
+#rpm --import http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro &> /dev/null
+#rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm &> /dev/null
 #yum install http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm
 #systemctl stop firewalld.service && /bin/systemctl disable firewalld.service 
 
@@ -25,6 +32,13 @@ yum install MariaDB-server MariaDB-client -y &> /dev/null
 systemctl start mariadb 1>/dev/null
 systemctl enable mariadb &> /dev/null
 mysql_upgrade 1>/dev/null
+
+#PMA
+#wget https://files.phpmyadmin.net/phpMyAdmin/4.9.5/phpMyAdmin-4.9.5-all-languages.zip
+#unzip phpMyAdmin-4.9.5-all-languages.zip
+#rm -Rfv phpMyAdmin-4.9.5-all-languages.zip
+#rm -Rfv /usr/share/phpMyAdmin
+#mv phpMyAdmin-4.9.5-all-languages /usr/share/phpMyAdmin
 
 cat >/etc/my.cnf << HERE 
 [mysqld]
@@ -55,11 +69,15 @@ echo "Fix MYSQL successfully"
 yum -y install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_12.x | sudo -E bash -
 yum -y install nodejs
-
-yum install ffmpeg ffmpeg-devel nano mc htop atop iftop lsof bzip2 traceroute gdisk php74-php-curl php74-php-mbstring  php74-php-xml php74-php-gd php74-php-fileinfo php74-php-exif php74-php-intl php74-php-zip php74-php-mysqli php74-php-curl php74-php-ctype php74-php-openssl php74-php-pdo php74-php-opcache php74-php-simplexml php74-php-mysql php72-php-mbstring php72-php-xml php72-php-gd php72-php-fileinfo php72-php-intl php72-php-zip php72-php-mysqli php72-php-curl php72-php-ctype php72-php-openssl php72-php-pdo php72-php-exif php72-php-opcache php72-php-simplexml php72-php-mysql php72-php-curl php74-php-xdebug php73-php-xdebug php72-php-xdebug php70-php-xdebug php72-php-soap php73-php-soap php74-php-soap -y &> /dev/null
+yum localinstall --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm -y &> /dev/null
+yum install ffmpeg ffmpeg-devel nano git mc htop atop iftop lsof bzip2 traceroute gdisk -y &> /dev/null
+yum install php74-php-curl php74-php-mbstring  php74-php-xml php74-php-gd php74-php-fileinfo php74-php-exif php74-php-intl php74-php-zip php74-php-mysqli php74-php-curl php74-php-ctype php74-php-openssl php74-php-pdo php74-php-opcache php74-php-simplexml php74-php-mysql php74-php-soap php74-php-xdebug -y &> /dev/null
+yum install php72-php-mbstring php72-php-xml php72-php-gd php72-php-fileinfo php72-php-intl php72-php-zip php72-php-mysqli php72-php-curl php72-php-ctype php72-php-openssl php72-php-pdo php72-php-exif php72-php-opcache php72-php-simplexml php72-php-mysql php72-php-curl php72-php-xdebug php72-php-soap -y &> /dev/null
+yum install php73-php-curl php73-php-mbstring  php73-php-xml php73-php-gd php73-php-fileinfo php73-php-exif php73-php-zip php73-php-mysqli php73-php-curl php73-php-ctype php73-php-openssl php73-php-soap php73-php-intl php73-php-pdo php73-php-opcache php73-php-simplexml php73-php-mysql php73-php-xdebug -y &> /dev/null
 
 wget https://raw.githubusercontent.com/it-toppp/sk-php-selector/master/sk-php-selector2.sh &> /dev/null
 chmod +x sk-php-selector2.sh && bash sk-php-selector2.sh php70 php71 php72 php73 &> /dev/null
+mv /etc/httpd/conf.modules.d/15-php73-php.conf /etc/httpd/conf.modules.d/14-php73-php.conf 
 
 wget http://downloads2.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
 tar zxf ioncube_loaders_lin_x86-64.tar.gz 
@@ -84,9 +102,6 @@ max_input_vars = 3000
 max_input_time = 6000
 zlib.output_compression = Off
 memory_limit = 1000M
-[Zend Modules]
-zend_extension = /usr/local/ioncube/ioncube_loader_lin_7.4.so
-zend_extension_ts = /usr/local/ioncube/ioncube_loader_lin_7.4_ts.so
 HERE
 
 cat >>/etc/opt/remi/php70/php.ini << HERE 
@@ -153,10 +168,10 @@ zend_extension = /usr/local/ioncube/ioncube_loader_lin_7.3.so
 zend_extension_ts = /usr/local/ioncube/ioncube_loader_lin_7.3_ts.so
 HERE
 
-
 systemctl restart httpd 1>/dev/null
 echo "Fix PHP and HTTPD successfully"
 
+echo "Fix PMA successfully"
 #nginx
 sed -i 's|client_max_body_size            256m|client_max_body_size            2048m|' /etc/nginx/nginx.conf
 sed -i 's|worker_connections  1024;|worker_connections  2024;|' /etc/nginx/nginx.conf
@@ -190,11 +205,23 @@ HERE
 
 yum install curl -y 1>/dev/null
 
+#EXIM
+wget https://ca1.dynanode.net/exim-4.93-3.el7.x86_64.rpm
+rpm -Uvh --oldpackage exim-4.93-3.el7.x86_64.rpm
+sed -i 's|  drop    condition     = ${if isip{$sender_helo_name}}|#  drop    condition     = ${if isip{$sender_helo_name}}|' /etc/exim/exim.conf
+sed -i 's|          message       = Access denied - Invalid HELO name (See RFC2821 4.1.3)|#          message       = Access denied - Invalid HELO name (See RFC2821 4.1.3)|' /etc/exim/exim.conf 
+systemctl restart exim
+
+#SWAP
+wget https://raw.githubusercontent.com/it-toppp/Swap/master/swap.sh -O swap && sh swap 2048
+
 echo "Full installation completed [ OK ]"
 
-
-DOMAIN=$(ls /home/admin/web)
-PASSWDDB=$(openssl rand -base64 12)
+#SITE
+#if [ "$DIG_IP" = "$IP" ]; then echo  "DNS lookup for $DOMAIN resolved to $DIG_IP, enabled ssl"
+#/usr/local/vesta/bin/v-add-letsencrypt-domain admin $DOMAIN www.$DOMAIN "yes"
+#fi
+#/usr/local/vesta/bin/v-add-database admin def def $PASSWD mysql
 
 echo "Which script use?"
 echo "   1) PLAYTUBE"
@@ -210,46 +237,73 @@ read -p "Script [1]: " script
     done
     case "$script" in
 1|"")
-cd /home/admin/web/$DOMAIN/public_html/ && wget http://ss.ultahost.com/playtube.zip
-rm -Rfv robots.txt index.html && unzip playtube.zip && rm -Rfv __MACOSX playtube.zip
-chmod -R 777 config.php upload assets/import/ffmpeg/ffmpeg && 
-sed -i 's|domain.com|'$DOMAIN'|' .htaccess
+cd /home/admin/web/$DOMAIN/public_html/ && rm -Rfv robots.txt index.html
+wget http://ss.ultahost.com/playtube.zip && unzip playtube.zip && chmod -R 777 config.php upload assets/import/ffmpeg/ffmpeg nodejs/config.json
+rm -Rfv __MACOSX playtube.zip
+sed -i 's|domain.com|'$DOMAIN'/|' .htaccess
 #/usr/local/vesta/bin/v-add-database admin playtube playtube $PASSWDDB mysql
 #mysql -uadmin_playtube -p$PASSWDDB admin_playtube < playtube.sql
 chown -R admin:admin /home/admin/web
-
 echo "  installation complete"
 ;;
 2)
-cd /home/admin/web/$DOMAIN/public_html/
-wget http://ss.ultahost.com/wowonder.zip && rm -Rfv robots.txt index.html && unzip wowonder.zip
-rm -Rfv __MACOSX wowonder.zip && chmod -R 777 cache upload config.php && chown -R admin:admin /home/admin/web
-sed -i 's|domain.com|'$DOMAIN'|' .htaccess
+cd /home/admin/web/$DOMAIN/public_html/ && rm -Rfv robots.txt index.html
+wget http://ss.ultahost.com/wowonder.zip  && unzip wowonder.zip && chmod -R 777 cache upload config.php && chown -R admin:admin /home/admin/web
+rm -Rfv __MACOSX wowonder.zip &&
+sed -i 's|domain.com|'$DOMAIN'/|' .htaccess
 echo "  installation complete"
 ;;
 3)
-cd /home/admin/web/$DOMAIN/public_html/ && wget http://ss.ultahost.com/deepsound.zip
-rm -Rfv robots.txt index.html && unzip deepsound.zip && rm -Rfv __MACOSX deepsound.zip  
-chmod -R 777 upload config.php ffmpeg/ffmpeg && chown -R admin:admin /home/admin/web
+cd /home/admin/web/$DOMAIN/public_html/ && rm -Rfv robots.txt index.html
+wget http://ss.ultahost.com/deepsound.zip && unzip deepsound.zip  && chmod -R 777 upload config.php ffmpeg/ffmpeg && chown -R admin:admin /home/admin/web
+rm -Rfv __MACOSX deepsound.zip
+sed -i 's|domain.com|'$DOMAIN'/|' .htaccess
 echo "  installation complete"
 ;;
 4)
-cd /home/admin/web/$DOMAIN/public_html/ && wget http://ss.ultahost.com/quickdate.zip 
-rm -Rfv robots.txt index.html unzip quickdate.zip && rm -Rfv __MACOSX quickdate.zip 
-chmod -R 777 upload cache config.php ffmpeg/ffmpeg 
-chown -R admin:admin /home/admin/web
-sed -i 's|domain.com|'$DOMAIN'|' .htaccess
+cd /home/admin/web/$DOMAIN/public_html/ && rm -Rfv robots.txt index.html
+wget http://ss.ultahost.com/quickdate.zip && unzip quickdate.zip && chmod -R 777 upload cache config.php ffmpeg/ffmpeg && chown -R admin:admin /home/admin/web
+&& rm -Rfv __MACOSX quickdate.zip
+sed -i 's|domain.com|'$DOMAIN'/|' .htaccess
 echo "  installation complete"
 ;;
 5)
-cd /home/admin/web/$DOMAIN/public_html/
-wget http://ss.ultahost.com/pixelphoto.zip
-rm -Rfv robots.txt index.html && unzip pixelphoto.zip && rm -Rfv __MACOSX pixelphoto.zip 
-chmod -R 777 config.php sys/ffmpeg/ffmpeg ffmpeg/ffmpeg && chown -R admin:admin /home/admin/web
-sed -i 's|domain.com|'$DOMAIN'|' .htaccess
+cd /home/admin/web/$DOMAIN/public_html/ && rm -Rfv robots.txt index.html 
+wget http://ss.ultahost.com/pixelphoto.zip && unzip pixelphoto.zip 
+chmod -R 777 sys/config.php sys/ffmpeg/ffmpeg && chown -R admin:admin /home/admin/web
+rm -Rfv __MACOSX pixelphoto.zip 
+sed -i 's|domain.com|'$DOMAIN'/|' .htaccess
 echo "  installation complete"
 ;;
 6)
 echo "  OK"
 ;;
 esac
+
+# Sending notification to admin email
+tmpfile=$(mktemp -p /tmp)
+
+echo '======================================================='
+echo -e "Installation is complete:
+
+Vesta Control Panel:
+    https://$DOMAIN:8083
+    username: admin
+    password: $PASSWD
+
+Filemanager:
+   https://$DOMAIN:8083/list/directory/?dir_a=/home/admin/web/$DOMAIN/public_html&dir_b=/home/admin
+
+FTP:
+   host: $IP
+   port: 21
+   username: admin
+   password: $PASSWD
+
+phpMyAdmin:
+   http://$IP/phpmyadmin
+   username = root
+   $(grep pass /root/.my.cnf)
+"
+echo '======================================================='
+cat $tmpfile
